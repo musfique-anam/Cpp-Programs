37eb3ba63c91cc6f054a62b5f92be47615787d9d
musfique-anam <musfique2002.personal@gmail.com>
2024-11-17 22:56:22 +0600

    Memory Address

--- Pointer.cpp
+++ Pointer.cpp
@@ -5,9 +5,9 @@ int main () {
     int num =10;
     int* ptr = &num;
 
-    cout << "Value: " << num << endl;
-    cout << "Address: " << ptr << endl;
-    cout << "Value through pointer: " << *ptr << endl;
+    cout << "Value : " << num << endl;
+    cout << "Address : " << ptr << endl;
+    cout << "Value through pointer : " << *ptr << endl;
 
     return 0;
 }
