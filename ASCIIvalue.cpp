b407fa3cd87939c52ea6cc8852b372ad1eff6aea
musfique-anam <musfique2002.personal@gmail.com>
2025-01-09 13:47:35 +0600

    ASCII value of a Character

---
+++ ASCIIvalue.cpp
@@ -0,0 +1,10 @@
+//Program to determine ASCII value
+#include <iostream>
+using namespace std;
+int main() {
+    char charInput;
+    cout << "Enter a character : ";
+    cin >> charInput;
+    cout << "ASCII value of this charcter is " << int (charInput);
+    return 0;
+}
